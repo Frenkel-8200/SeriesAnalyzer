3b34ef6725f5186def5ce83e57ea24f7f499ccb1
Aharon Frenkel <frenkelahron@gmail.com>
2025-05-13 23:20:50 +0300

    The program skeleton and method signatures

---
+++ Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SeriesAnalysis
+{
+    // Global variables
+    static double[] series;
+    static Dictionary<string, Func<dynamic>> menuOptions = new Dictionary<string, Func<dynamic>>
+    {
+        { "a", () => InputNewSeries() },
+        { "b", () => GetOriginalSeries() },
+        { "c", () => GetReverseSeries() },
+        { "d", () => GetSortedSeries() },
+        { "e", () => GetMaxSeries() },
+        { "f", () => GetMinSeries() },
+        { "g", () => GetAvgSeries() },
+        { "h", () => GetCountSeries() },
+        { "i", () => GetSumSeries() },
+        { "j", () => Exit() }
+    };
+
+    // Methods to perform operations on the series
+    static string InputNewSeries()
+    {
+    }
+
+    static double[] GetOriginalSeries()
+    {
+    }
+
+    static double[] GetReverseSeries()
+    {
+    }
+
+    static double[] GetSortedSeries()
+    {
+    }
+
+    static double GetMaxSeries()
+    {
+    }
+
+    static double GetMinSeries()
+    {
+    }
+
+    static double GetAvgSeries()
+    {
+    }
+
+    static int GetCountSeries()
+    {
+    }
+
+    static double GetSumSeries()
+    {
+    }
+
+    static string Exit()
+    {
+    }
+
+    static double[] ConvertArrayStringToDouble(string[] args)
+    {
+    }
+
+    static bool UpdateSeries(string message)
+    {
+    }
+
+    // Validation methods
+    static bool IsValidSeriesInput(string[] args)
+    {
+    }
+
+    static bool IsValidOptionInput(string choice)
+    {
+    }
+
+    static bool HasLeastThreeArgs(string[] args)
+    {
+    }
+
+    static bool IsNumeric(string[] args)
+    {
+    }
+
+    static bool IsPositiveNums(string[] args)
+    {
+    }
+
+
+    // Display methods
+    static void Print(string str)
+    {
+    }
+
+    static void Print(int num)
+    {
+    }
+    static void Print(double num)
+    {
+    }
+
+    static void Print(double[] arr)
+    {
+    }
+    static string PromptAndReadInput(string messsage)
+    {
+    }
+
+    // Menu methods
+    static string GetMenu()
+    {
+    }
+
+    static void HandleMenuOption()
+    {
+    }
+
+    // Main methods
+    static void Initialize(string[] args)
+    {
+    }
+
+    static void Run()
+    {
+    }
+
+    static void Main(string[] args)
+    {
+    }
+}
